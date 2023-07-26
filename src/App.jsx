8f141e800f1fae7c2c71b1ee006debad286d0d8a
DantDev <118768256+DantDev@users.noreply.github.com>
2023-07-26 19:39:55 +0100

    Changes made to the CSS and ".divider-mobile" added

--- src/App.jsx
+++ src/App.jsx
@@ -5,27 +5,26 @@ import diceIcon from "./assets/images/icon-dice.svg"
 
 function App() {
   const [advice,setAdvice] = React.useState([])
-  /*
-   React.useEffect(() => {
-    fetch("https://api.adviceslip.com/advice")
-    .then(res => res.json())
-    .then(data => setAdvice(data.slip))
-   },[])
-*/ 
-  const fetchAdvice = () => {
-  fetch("https://api.adviceslip.com/advice")
-  .then((res) => res.json())
-  .then((data) => setAdvice(data.slip));
-};
 
-React.useEffect(() => {
-fetchAdvice()
-}, []);
+  const fetchAdvice = async () => {
+    const response = await fetch("https://api.adviceslip.com/advice")
+    const data = await response.json()
+    setAdvice({
+      id: data.slip.id,
+      advice: data.slip.advice
+    })
+  }
+  
+  React.useEffect(() => {
+    fetchAdvice()
+  }, []);
+
   return (
     <div className='container'>
       <p className="advice-id">ADVICE #{advice.id}</p>
       <h1 className="advice-text">"{advice.advice}"</h1>
-      <img src={dividerDesk} />
+      <img className="divider-desktop" src={dividerDesk} />
+      <img className="divider-mobile" src={dividerMob} />
       <button onClick={fetchAdvice} className="advice-btn"><img src={diceIcon}/></button>
     </div>
   )
